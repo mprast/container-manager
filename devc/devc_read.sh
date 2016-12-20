d303ab8ccb9364aa3bc7539e4340560f620b208e
Matt Prast <mprast@gmail.com>
2016-12-19 18:15:25 -0800

    mount /containers when starting a dev container in read mode

--- devc/devc_read.sh
+++ devc/devc_read.sh
@@ -16,6 +16,15 @@ function vblog {
     fi
 }
 
+# TODO(mprast): factor this out into a preamble
+rkt_home_missing_msg=$(cat <<MSG
+Could not start dev container in record mode: RKT_HOME needs to be set. This should point to the data directory that rkt uses (usually /var/lib/rkt); it is usually set by the scripts that provision the machine. If you installed rkt manually on this host you will need to set it yourself.
+MSG
+)
+
+#shorthand; writes message to STDERR & exits if var is missing
+: "${RKT_HOME:? $rkt_home_missing_msg}"
+
 vblog "Checking if containers are already running using devc_list.sh..."
 $(dirname $0)/devc_list.sh > /dev/null
 if [ $? -eq 1 ]
@@ -28,6 +37,6 @@ fi
 sudo echo 'h' > /dev/null
 
 # TODO(mprast): factor out the name of the actual container.
-vblog "Starting a devc container in read mode by using 'rkt run' with stage1-fly. Running 'tail -f /dev/null' as a dummy process to keep the container going forever."
-sudo rkt run --insecure-options=image --stage1-name=coreos.com/rkt/stage1-fly:1.20.0 /home/mprast/.devc_build/devc.aci --exec "/usr/bin/tail" -- -f "/dev/null" &
+vblog "Starting a devc container in read mode by using 'rkt run' with stage1-fly. Running 'tail -f /dev/null' as a dummy process to keep the container going forever. Mounting the rkt running pod dir to /containers"
+sudo rkt run --insecure-options=image --stage1-name=coreos.com/rkt/stage1-fly:1.20.0 /home/mprast/.devc_build/devc.aci --volume containers,kind=host,source=$RKT_HOME/pods/run,recursive=true --mount volume=containers,target=/containers --exec "/usr/bin/tail" -- -f "/dev/null" &
 echo 'Dev container started!'
