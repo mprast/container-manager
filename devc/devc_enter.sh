cb71842bca9e0e01d40d94452ae93f0ba301eb9d
Matt Prast <mprast@gmail.com>
2019-08-19 23:28:15 -0700

    :hammer: move devc_enter to use buildah instead of rkt

--- devc/devc_enter.sh
+++ devc/devc_enter.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
-# Enters a running dev container. Looks for a container in 
-# 'write mode' first, then a container in 'read mode'.
-
-# exit if any command fails
-set -e 
+# Enters a running dev container. 
 
 # this is duplicated from ./devc_write.sh for now.
 # TODO(mprast): factor this method out
@@ -16,31 +12,15 @@ function vblog {
     fi
 }
 
-# TODO(mprast): factor out builddir name
-username=$(whoami)
-builddir="/home/$username/.devc_build"
-acbuilddir="$builddir/.acbuild"
-if [ -e $acbuilddir ]
+vblog "Checking if containers are already running using devc_status.sh..."
+$(dirname $0)/devc_status.sh > /dev/null
+if [ $? -eq 0 ]
    then
-   pushd $builddir > /dev/null
-   vblog "Entering write container with acbuild run '/bin/bash'... "
-   sudo acbuild run --engine chroot "/bin/bash"
-   popd > /dev/null
-else
-   # TODO(mprast): rewrite this to be more stable - use the 
-   # rkt metadata api
-
-   # this gets the hash of any running fedora containers 
-   # and passes them to rkt enter. assumes there's only 
-   # one fedora container running on the box.
-   devc_sha=$(rkt list | perl -ane 'print $F[0] if $F[2] =~ /fedora:latest/ && $F[3] =~ /running/') 
-   if [ ! "$devc_sha" ]
-      then
-      echo "no running dev container found! (no write container since $acbuilddir doesn't exist, no read 
-      container since no running fedora container found by rkt list)." >&1
-      exit 1
-   fi
-
-   vblog "Entering read container (id $devc_sha) with rkt enter..." 
-   sudo rkt enter $devc_sha
+   echo 'No devc container is currently running. Try devc_up.sh.' >&2
+   exit 1
 fi
+
+vblog "Entering devc container with 'buildah run'..."
+# isolation must be 'chroot' so the bind mounts we set up in 
+# devc_up will work.
+sudo buildah run --tty --isolation chroot devc_local-working-container /bin/bash 
